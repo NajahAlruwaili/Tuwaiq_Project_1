39c68cca4830996b75ce7b3fc1cbd0ceee6cf40f
NajahAlruwaili <Najah.Nafea.rae.js@tuwaiq.edu.sa>
2021-10-17 16:47:11 +0300

    done part 1

---
+++ store.js
@@ -0,0 +1,18 @@
+const signUp = document.querySelector(".signUp");
+
+   signUp.style.display="none";
+
+function toSignUp() {
+    //console.log ("ok");
+   signUp.style.display= "flex";
+}
+
+const Contact = document.querySelector(".Contact");
+
+Contact.style.display="none";
+
+function toContact() {
+    console.log ("ok");
+    Contact.style.display= "flex";
+}
+
